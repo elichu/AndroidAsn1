db3cbd97a056df4fa82e7bd363179ae6b9a85104
elichu <elichu0@gmail.com>
2017-09-25 16:18:26 -0700

    asn mostly complete, requires language support

---
+++ app/src/main/java/ca/bcit/ass1/elisachu_philipshin/InputActivity.java
@@ -0,0 +1,102 @@
+package ca.bcit.ass1.elisachu_philipshin;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
+public class InputActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input);
+
+        Button button1 = (Button) findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float inputDecimal = 0;
+                EditText input = (EditText) findViewById(R.id.input);
+                if(input.getText().toString().isEmpty()){
+                    return;
+                }
+                inputDecimal = Float.parseFloat(input.getText().toString());
+
+                Spinner temperatureSpinner = (Spinner) findViewById(R.id.temperature);
+                String selection = String.valueOf(temperatureSpinner.getSelectedItem());
+                String convertedTemp = getTemperature(selection, inputDecimal);
+
+                Intent intent = new Intent(InputActivity.this, OutputActivity.class);
+                intent.putExtra("result", convertedTemp);
+                startActivity(intent);
+            }
+
+        });
+
+        Button button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent homeIntent = new Intent(Intent.ACTION_MAIN);
+                homeIntent.addCategory( Intent.CATEGORY_HOME );
+                homeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(homeIntent);
+            }
+        });
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+    }
+
+    public String getTemperature(String conversion, float input) {
+        DecimalFormat df = new DecimalFormat("##.##");
+        df.setRoundingMode(RoundingMode.DOWN);
+
+        String result = df.format(input);
+        switch (conversion) {
+            case "Celsius to Fahrenheit":
+                result = result + " °C converts to "
+                        + df.format((input*1.8) + 32) + " °F";
+                break;
+            case "Celsius to Kelvin":
+                result = result + " °C converts to "
+                        + df.format((input+273.15)) + " °K";
+                break;
+            case "Fahrenheit to Kelvin":
+                result = result + " °F converts to "
+                        + df.format((input+459.67) * .5555556) + " °K";
+                break;
+            case "Fahrenheit to Celsius":
+                result = result + " °F converts to "
+                        + df.format((input-32)* .5555556) + " °C";
+                break;
+            case "Kelvin to Celsius":
+                result = result + " °K converts to "
+                        + df.format((input-273.15)) + " °C";
+                break;
+            case "Kelvin to Fahrenheit":
+                result = result + " °K converts to "
+                        + df.format((input*1.8) - 459.67) + " °F";
+                break;
+            default:
+                result = "Invalid Input";
+        }
+
+        return result;
+    }
+}
---
+++ app/src/main/java/ca/bcit/ass1/elisachu_philipshin/OutputActivity.java
@@ -0,0 +1,51 @@
+package ca.bcit.ass1.elisachu_philipshin;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by E on 2017-09-25.
+ */
+
+public class OutputActivity extends Activity {
+
+    public static final String EXTRA_MESSAGE = "result";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_output);
+
+        Intent intent = getIntent();
+        String messageText = intent.getStringExtra(EXTRA_MESSAGE);
+        TextView messageView = (TextView) findViewById(R.id.output2);
+        messageView.setText(messageText);
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startMain = new Intent(OutputActivity.this, InputActivity.class);
+                //startMain.addCategory(Intent.CATEGORY_HOME);
+                //startMain.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                startActivity(startMain);
+            }
+        });
+        Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent homeIntent = new Intent(Intent.ACTION_MAIN);
+                homeIntent.addCategory( Intent.CATEGORY_HOME );
+                homeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(homeIntent);
+            }
+        });
+
+    }
+
+}
